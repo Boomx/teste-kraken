2f4da9bfed2400106a26b853965ac3c4dad1a34c
Denilson Junior <jundenilson@gmail.com>
2016-03-17 22:47:20 -0300

    adicoes de leves

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject player;
+    private Transform cameraTarget;
+
+    // Use this for initialization
+
+    public void Start() {
+        //pedreiro = GameObject.Find("Pedreiro");
+        player = GameObject.FindGameObjectWithTag("Player");
+        cameraTarget = player.transform;
+        //maxXAndY = new Vector2(pedreiro.GetComponent<Mapa>().tamMapMax().x - 12.5f, pedreiro.GetComponent<Mapa>().tamMapMax().y - 7.0f);
+        //minXAndY = new Vector2(pedreiro.GetComponent<Mapa>().tamMapMin().x + 12.5f, pedreiro.GetComponent<Mapa>().tamMapMin().y + 7.0f);
+    }
+
+    //bool CheckXMargin() {
+    //    return Mathf.Abs(transform.position.x - cameraTarget.position.x) > xMargin;
+    //}
+
+    //bool CheckYMargin() {
+    //    return Mathf.Abs(transform.position.y - cameraTarget.position.y) > yMargin;
+    //}
+
+    void FixedUpdate() {
+        TrackPlayer();
+    }
+
+    void TrackPlayer() {
+        //float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        //if (CheckXMargin()) {
+            //targetX = Mathf.Lerp(transform.position.x, cameraTarget.position.x, 1 * Time.deltaTime);
+        //}
+
+        //if (CheckYMargin()) {
+            targetY = Mathf.Lerp(transform.position.y, cameraTarget.position.y, 1 * Time.deltaTime);
+        //}
+
+        //targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+        //targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+        transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarScript : MonoBehaviour {
+    [SerializeField]
+    GameObject RightWhells;
+    [SerializeField]
+    GameObject LeftWhells;
+
+    float TrainAngle;
+    public float MultiBreak;
+    public float MultiAccel;
+
+	void Update () {
+        Vector2 TrainAngle = transform.TransformDirection(Vector3.up);
+        Vector2 InverseTrainAngle = transform.TransformDirection(Vector3.down);
+
+        Debug.Log("Vetor direcao" + transform.TransformDirection(Vector3.down));
+
+        GetComponent<Rigidbody2D>().AddForce(TrainAngle*MultiAccel);
+        if (Input.GetAxis("Horizontal")>0) //botâo Direito
+            GetComponent<Rigidbody2D>().AddForceAtPosition(InverseTrainAngle * MultiBreak, 
+                RightWhells.transform.position);
+        if (Input.GetAxis("Horizontal") < 0) //botâo Esquerdo
+            GetComponent<Rigidbody2D>().AddForceAtPosition(InverseTrainAngle * MultiBreak, 
+                LeftWhells.transform.position);
+    }
+}
